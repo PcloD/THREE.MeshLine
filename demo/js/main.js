62c7aeb2a6575a2408ca773fbf351ed9e72d5cc3
Mugen87 <michael.herzog@human-interactive.org>
2017-03-18 18:13:56 +0100

    Usage of new built-in MeshLine

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,9 +1,7 @@
-'use strict'
-
 var container = document.getElementById( 'container' );
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, .1, 1000 );
+var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set( 0, 0, -10 );
 
 var renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true });
@@ -15,8 +13,6 @@ var controls = new THREE.OrbitControls( camera, renderer.domElement );
 var clock = new THREE.Clock();
 
 var lines = [];
-var resolution = new THREE.Vector2( window.innerWidth, window.innerHeight );
-var strokeTexture;
 
 var Params = function() {
 	this.curves = true;
@@ -24,17 +20,13 @@ var Params = function() {
 	this.amount = 100;
 	this.lineWidth = 10;
 	this.taper = 'parabolic';
-	this.strokes = false;
-	this.sizeAttenuation = false;
 	this.animateWidth = false;
-	this.spread = false;
 	this.autoRotate = true;
 	this.autoUpdate = true;
-    this.animateVisibility=false;
 	this.update = function() {
 		clearLines();
 		createLines();
-	}
+	};
 };
 
 var params = new Params();
@@ -54,26 +46,18 @@ window.addEventListener( 'load', function() {
 	gui.add( params, 'amount', 1, 1000 ).onChange( update );
 	gui.add( params, 'lineWidth', 1, 20 ).onChange( update );
 	gui.add( params, 'taper', [ 'none', 'linear', 'parabolic', 'wavy' ] ).onChange( update );
-	gui.add( params, 'strokes' ).onChange( update );
-	gui.add( params, 'sizeAttenuation' ).onChange( update );
 	gui.add( params, 'autoUpdate' ).onChange( update );
 	gui.add( params, 'update' );
 	gui.add( params, 'animateWidth' );
-	gui.add( params, 'spread' );
 	gui.add( params, 'autoRotate' );
-    gui.add( params, 'animateVisibility' );
 
-	var loader = new THREE.TextureLoader();
-	loader.load( 'assets/stroke.png', function( texture ) {
-		strokeTexture = texture;
-		init()
-	} );
+	init();
 
 } );
 
 var TAU = 2 * Math.PI;
 var hexagonGeometry = new THREE.Geometry();
-for( var j = 0; j < TAU - .1; j += TAU / 100 ) {
+for( var j = 0; j < TAU - 0.1; j += TAU / 100 ) {
 	var v = new THREE.Vector3();
 	v.set( Math.cos( j ), Math.sin( j ), 0 );
 	hexagonGeometry.vertices.push( v );
@@ -87,22 +71,21 @@ function createCurve() {
 	var rMax = 10;
 	var origin = new THREE.Vector3( Maf.randomInRange( -rMin, rMin ), Maf.randomInRange( -rMin, rMin ), Maf.randomInRange( -rMin, rMin ) );
 
-	s.inc = .001;
-	s.p0 = new THREE.Vector3( .5 - Math.random(), .5 - Math.random(), .5 - Math.random() );
+	s.inc = 0.001;
+	s.p0 = new THREE.Vector3( 0.5 - Math.random(), 0.5 - Math.random(), 0.5 - Math.random() );
 	s.p0.set( 0, 0, 0 );
-	s.p1 = s.p0.clone().add( new THREE.Vector3( .5 - Math.random(), .5 - Math.random(), .5 - Math.random() ) );
-	s.p2 = s.p1.clone().add( new THREE.Vector3( .5 - Math.random(), .5 - Math.random(), .5 - Math.random() ) );
-	s.p3 = s.p2.clone().add( new THREE.Vector3( .5 - Math.random(), .5 - Math.random(), .5 - Math.random() ) );
+	s.p1 = s.p0.clone().add( new THREE.Vector3( 0.5 - Math.random(), 0.5 - Math.random(), 0.5 - Math.random() ) );
+	s.p2 = s.p1.clone().add( new THREE.Vector3( 0.5 - Math.random(), 0.5 - Math.random(), 0.5 - Math.random() ) );
+	s.p3 = s.p2.clone().add( new THREE.Vector3( 0.5 - Math.random(), 0.5 - Math.random(), 0.5 - Math.random() ) );
 	s.p0.multiplyScalar( rMin + Math.random() * rMax );
 	s.p1.multiplyScalar( rMin + Math.random() * rMax );
 	s.p2.multiplyScalar( rMin + Math.random() * rMax );
 	s.p3.multiplyScalar( rMin + Math.random() * rMax );
 
 	s.calculate();
-	var geometry = new THREE.Geometry();
 	s.calculateDistances();
-	//s.reticulate( { distancePerStep: .1 });
 	s.reticulate( { steps: 500 } );
+
  	var geometry = new THREE.Geometry();
 
 	for( var j = 0; j < s.lPoints.length - 1; j++ ) {
@@ -142,43 +125,27 @@ function clearLines() {
 
 function makeLine( geo ) {
 
-	var g = new MeshLine();
+	var meshLine;
 
-	switch( params.taper ) {
-		case 'none': g.setGeometry( geo ); break;
-		case 'linear': g.setGeometry( geo, function( p ) { return 1 - p; } ); break;
-		case 'parabolic': g.setGeometry( geo, function( p ) { return 1 * Maf.parabola( p, 1 )} ); break;
-		case 'wavy': g.setGeometry( geo, function( p ) { return 2 + Math.sin( 50 * p ) } ); break;
+	var material = new THREE.MeshBasicMaterial( { color: new THREE.Color( colors[ ~~Maf.randomInRange( 0, colors.length ) ] ), meshLine: true, meshLineWidth: 10 } );
+
+	switch ( params.taper ) {
+		case 'none': meshLine = new THREE.MeshLine( geo, material ); break;
+		case 'linear': meshLine = new THREE.MeshLine( geo, material, function( p ) { return 1 - p; } ); break;
+		case 'parabolic': meshLine = new THREE.MeshLine( geo, material, function( p ) { return 1 * Maf.parabola( p, 1 ); } ); break;
+		case 'wavy': meshLine = new THREE.MeshLine( geo, material,  function( p ) { return 2 + Math.sin( 50 * p ); } ); break;
 	}
 
-	var material = new MeshLineMaterial( {
-		map: strokeTexture,
-		useMap: params.strokes,
-		color: new THREE.Color( colors[ ~~Maf.randomInRange( 0, colors.length ) ] ),
-		opacity: 1,//params.strokes ? .5 : 1,
-		dashArray: new THREE.Vector2( 10, 5 ),
-		resolution: resolution,
-		sizeAttenuation: params.sizeAttenuation,
-		lineWidth: params.lineWidth,
-		near: camera.near,
-		far: camera.far,
-		depthWrite: false,
-		depthTest: !params.strokes,
-		alphaTest: params.strokes ? .5 : 0,
-		transparent: true,
-		side: THREE.DoubleSide
-	});
-	var mesh = new THREE.Mesh( g.geometry, material );
-	if( params.spread || params.circles ) {
+	if( params.circles ) {
 		var r = 50;
-		mesh.position.set( Maf.randomInRange( -r, r ), Maf.randomInRange( -r, r ), Maf.randomInRange( -r, r ) );
+		meshLine.position.set( Maf.randomInRange( -r, r ), Maf.randomInRange( -r, r ), Maf.randomInRange( -r, r ) );
 		var s = 10 + 10 * Math.random();
-		mesh.scale.set( s,s,s );
-		mesh.rotation.set( Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI );
+		meshLine.scale.set( s,s,s );
+		meshLine.rotation.set( Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI );
 	}
-	scene.add( mesh );
+	scene.add( meshLine );
 
-	lines.push( mesh );
+	lines.push( meshLine );
 
 }
 
@@ -193,33 +160,12 @@ function init() {
 function createLine() {
 	if( params.circles ) makeLine( hexagonGeometry );
 	if( params.curves ) makeLine( createCurve() );
-	//makeLine( makeVerticalLine() );
-	//makeLine( makeSquare() );
 }
 
 function createLines() {
 	for( var j = 0; j < params.amount; j++ ) {
 		createLine();
 	}
-}
-
-function makeVerticalLine() {
-	var g = new THREE.Geometry()
-	var x = ( .5 - Math.random() ) * 100;
-	g.vertices.push( new THREE.Vector3( x, -10, 0 ) );
-	g.vertices.push( new THREE.Vector3( x, 10, 0 ) );
-	return g;
-}
-
-function makeSquare() {
-	var g = new THREE.Geometry()
-	var x = ( .5 - Math.random() ) * 100;
-	g.vertices.push( new THREE.Vector3( -1, -1, 0 ) );
-	g.vertices.push( new THREE.Vector3( 1, -1, 0 ) );
-	g.vertices.push( new THREE.Vector3( 1, 1, 0 ) );
-	g.vertices.push( new THREE.Vector3( -1, 1, 0 ) );
-	g.vertices.push( new THREE.Vector3( -1, -1, 0 ) );
-	return g;
 }
 
 function onWindowResize() {
@@ -231,8 +177,6 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 
 	renderer.setSize( w, h );
-
-	resolution.set( w, h );
 
 }
 
@@ -248,9 +192,14 @@ function render(time) {
 	var delta = clock.getDelta();
 	var t = clock.getElapsedTime();
 	lines.forEach( function( l, i ) {
-		if( params.animateWidth ) l.material.uniforms.lineWidth.value = params.lineWidth * ( 1 + .5 * Math.sin( 5 * t + i ) );
-		if( params.autoRotate ) l.rotation.y += .125 * delta;
-      l.material.uniforms.visibility.value= params.animateVisibility ? (time/3000) % 1.0 : 1.0;
+		if ( params.animateWidth ) {
+
+			l.material.meshLineWidth = params.lineWidth * ( 1 + 0.5 * Math.sin( 5 * t + i ) );
+
+		}
+		if( params.autoRotate ) {
+			l.rotation.y += 0.125 * delta;
+		}
 	} );
 
 	renderer.render( scene, camera );
